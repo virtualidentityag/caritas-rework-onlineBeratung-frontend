cba7d2307a6f45e4452d2b3afec76726961a550f
tkuzynow <tomasz.kuzynowski@gmail.com>
2025-06-13 15:49:04 +0200

    chore: remove session delete button

--- src/components/sessionMenu/SessionMenu.tsx
+++ src/components/sessionMenu/SessionMenu.tsx
@@ -54,7 +54,6 @@ import { ReactComponent as CallOnIcon } from '../../resources/img/icons/call-on.
 import { ReactComponent as CameraOnIcon } from '../../resources/img/icons/camera-on.svg';
 import { ReactComponent as CalendarMonthPlusIcon } from '../../resources/img/icons/calendar-plus.svg';
 import { supportsE2EEncryptionVideoCall } from '../../utils/videoCallHelpers';
-import DeleteSession from '../session/DeleteSession';
 import { Text } from '../text/Text';
 import { useSearchParam } from '../../hooks/useSearchParams';
 import { useAppConfig } from '../../hooks/useAppConfig';
